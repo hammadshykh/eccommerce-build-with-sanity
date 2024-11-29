0a649079ed854b0b7621c607ecc4f217961e2e3b
hammadshykh <hammadyounus.508@gmail.com>
2024-11-29 18:38:50 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+ reactStrictMode: false,
+ images: {
+  remotePatterns: [
+   {
+    protocol: "https",
+    hostname: "cdn.sanity.io",
+   },
+   {
+    protocol: "https",
+    hostname: "firebasestorage.googleapis.com",
+   },
+  ],
+  domains: ["firebasestorage.googleapis.com", "cdn.sanity.io"],
+ },
+};
 
 export default nextConfig;
